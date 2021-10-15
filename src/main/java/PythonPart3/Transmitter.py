949a90fbbaaa3e21700841d462143ec50d037fe5
Xiaoyuan Du <duxy1@shanghaitech.edu.cn>
2021-10-15 11:35:46 +0800

    111

--- src/main/java/PythonPart3/Transmitter.py
+++ src/main/java/PythonPart3/Transmitter.py
@@ -9,77 +9,48 @@ from scipy.fft import fft
 
 def transmit(file_name):
     channels = 1
-    format = pyaudio.paFloat32
     sample_rate = 48000
-    P = pyaudio.PyAudio()
     carrier_freq = 2000
     carrier0 = np.sin(2 * np.pi * carrier_freq * np.arange(0, 0.001, 1 / sample_rate)).astype(np.float32)
     carrier1 = -(np.sin(2 * np.pi * carrier_freq * np.arange(0, 0.001, 1 / sample_rate))).astype(np.float32)
     with open(file_name, 'r') as file:
         file_data = file.read()
-    stream = P.open(format=format,
-                    channels=channels,
-                    rate=sample_rate,
-                    output=True)
     input_index = 0
-    symbols_in_frame = 500
+    symbols_in_frame = 100
     frame_num = int(len(file_data) / symbols_in_frame)
-    if frame_num * 1000 < len(file_data):
+    if frame_num * 100 < len(file_data):
         frame_num += 1
     header = gen_header()
+    data = []
     for i in range(frame_num):
-        stream.write(header.tobytes())
+        data.append(header)
         for j in range(symbols_in_frame):
-            if (input_index < len(file_data)):
-                input = file_data[input_index]
+            if file_data[input_index] == '0':
+                data.append(carrier0)
             else:
-                input = '0'
-            if input == '0':
-                stream.write(carrier0.tobytes())
-            else:
-                stream.write(carrier1.tobytes())
+                data.append(carrier1)
             input_index += 1
-
+    data = np.concatenate(data)
+    sd.default.channels = 1
+    sd.play(data, samplerate=sample_rate)
+    sd.wait()
 
 def gen_header():
     ''' header for 0.01 second'''
-    # header_length = 480
-    # sample_rate = 48000
-    # F0 = 1875
-    # dF = 46.875
-    # header_index = [0, 3, 4, 7, 8, 11, 12, 15, 16, 19, 20, 23, 24, 27, 28, 31]
-    # header = np.zeros(48000, dtype=np.float32)
-    # for i in header_index:
-    #     if i % 2 == 0:
-    #         header += np.sin(2*np.pi*(F0 + i * dF) * np.arange(0, 1, 1 / sample_rate)).astype(np.float32)
-    #     else:
-    #         header += np.sin(2*np.pi*(F0 + i * dF) * np.arange(0, 1, 1 / sample_rate)).astype(np.float32)
-    # corr = signal.correlate(header, header)
-    # y = fft(header)
-    # y = abs(y)
-    # y = y[0:4000]
-    # plt.plot(range(len(y)), y)
-    # plt.show()
-    t = np.linspace(0,1, 48000,endpoint = True,dtype = np.float32)
-    t=t[0:480]
-    f_p = np.concatenate([np.linspace(2500, 8000, 240), np.linspace(8000,2500, 240)])
-    header = (np.sin(2*np.pi*integrate.cumtrapz(f_p, t))).astype(np.float32)
-    corr = signal.correlate(header, header)
-    max = np.max(abs(corr))
-    avg = np.average(abs(corr))
-    print(max / avg)
+    t = np.linspace(0, 1, 48000, endpoint=True, dtype=np.float32)
+    t = t[0:480]
+    f_p = np.concatenate([np.linspace(2500, 8000, 240), np.linspace(8000, 2500, 240)])
+    header = (np.sin(2 * np.pi * integrate.cumtrapz(f_p, t))).astype(np.float32)
     return header
 
 
-# start = time.time()
-# transmit("INPUT.txt")
-# end = time.time()
-# print("Transmitting time: ", end - start)
+start = time.time()
+transmit("INPUT.txt")
+end = time.time()
+print("Transmitting time: ", end - start)
 # header = gen_header()
 # sd.play(header, 48000)
 # sd.wait()
-carrier0 = np.sin(2 * np.pi * 2000 * np.arange(0, 0.001, 1 / 48000)).astype(np.float32)
-carrier1 = -np.sin(2 * np.pi * 2000 * np.arange(0, 0.001, 1 / 48000)).astype(np.float32)
-y = abs(fft(carrier0))
-plt.plot(range(len(y)), y)
-plt.show()
+# sd.sleep(1000)
+# sd.play(header, 48000)
+# sd.wait()
